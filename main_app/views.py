fbf10bf8c8bba6a463d3b48720b92a9f74e5776a
Paul Burgess <paul@burgess.io>
2024-04-11 14:36:51 -0400

    Add Home, About, and All Finches

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+finches = [
+  {'name': 'American Goldfinch', 'subtype': 'goldfinch', 'description': 'yellow with black wings', 'size': 'small'},
+  {'name': 'Rose-Breasted Grosbeak', 'subtype': 'grosbeak', 'description': 'black back with a dark pink breast', 'size': 'large'},
+]
+
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {
+        'finches' : finches
+    })
